fb48efa422ff0c2dde8e3666fb29a1ce9eea443c
Roger Sperberg <rsperberg@gmail.com>
2013-09-29 14:58:49 -0400

    Made statusbar hidden
    Used info from RM google group
    https://groups.google.com/forum/#!topic/rubymotion/UUrVJSM5zZM

--- Rakefile
+++ Rakefile
@@ -13,10 +13,11 @@ Motion::Project::App.setup do |app|
   app.device_family = [:iphone]
   app.identifier="ws.spress.romancalc"
   app.interface_orientations = [:portrait]
-  app.sdk_version="6.1"
-  app.deployment_target = "6.1"
+  # app.sdk_version="6.1"
+  # app.deployment_target = "6.1"
   app.detect_dependencies = false
   app.version = "0.5"
+  app.info_plist['UIViewControllerBasedStatusBarAppearance'] = false
 #  following two lines are for version to run on ipad running version 5.1.1
 #  app.sdk_version="5.1"
 #  app.deployment_target = "5.1"
--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -2,6 +2,9 @@ class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
 
+    UIApplication.sharedApplication.setStatusBarHidden(true, false)
+    application.setStatusBarHidden(true, withAnimation:UIStatusBarAnimationSlide)
+    # taken from https://groups.google.com/forum/#!topic/rubymotion/UUrVJSM5zZM
     controller = RomanNumController.alloc.initWithNibName(nil, bundle:nil)
     @nav_controller = UINavigationController.alloc.initWithRootViewController(controller)
     @window.rootViewController = @nav_controller
